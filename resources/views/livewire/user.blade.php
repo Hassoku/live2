7184c1475775fcc62ec1d60cfade0fc26719c56f
Hassoku <36498663+Hassoku@users.noreply.github.com>
2021-06-24 21:55:39 +0500

    First Commit

---
+++ resources/views/livewire/user.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="relative flex items-top justify-center min-h-screen bg-dark-100 dark:bg-dark-900 sm:items-center py-4 sm:pt-0">
+
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+           <form>
+               <div class="row"></div>
+           </form>
+
+
+        </div>
+    </div>
+
+</div>
